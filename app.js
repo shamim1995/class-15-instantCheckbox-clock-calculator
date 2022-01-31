99edaf5ca1446d5550d8c17461e4b8dc10c77d56
Hamidul Islam shamim <mdhamidul2714@gmail.com>
2022-02-01 01:15:32 +0600

    Complete

--- app.js
+++ app.js
@@ -1,24 +1,94 @@
+/**
+ * this function for instant change get checkbox value
+ */
 
-const skill = document.querySelectorAll('input[type="checkbox"]')
+
+const skill = document.querySelectorAll('input[type="checkbox"]');
 const all_skills = document.querySelector('.all_skills')
 
-skill.forEach(skillData=>{
+skill.forEach(skillData =>{
 
-  skillData.addEventListener('change', function(){
+  skillData.addEventListener('change', function () {
+    
+    let skillChecked = document.querySelectorAll('input[type="checkbox"]:checked');
 
-      let skillValue = document.querySelectorAll('input[type="checkbox"]:checked');
-      let skill_arr = [];
+    let skill_arr = [];
 
-    skillValue.forEach( skills=>{
-       skill_arr.push(skills.value);
+    skillChecked.forEach(skillArry => {
+      skill_arr.push(skillArry.value)
     });
 
-    let list = ' ';
-    skill_arr.map( data => {
-     list += `<li class = "list-group-item"> ${data} </li>`;
-});
-all_skills.innerHTML = list;
-   
+    let list = '';
+    skill_arr.map(data=>{
+
+      list += `<li class="list-group-item"> ${data}</li>`
+
+    });
+
+    all_skills.innerHTML = list;
 
   });
+
 });
+
+
+/**
+ * this for clock
+ */
+
+const sec = document.querySelector('.sec');
+const min = document.querySelector('.min');
+const hour = document.querySelector('.hour');
+
+
+setInterval(() => {
+let time = new Date();
+let current_sec = time.getSeconds();
+let current_min = time.getMinutes();
+let current_hour = time.getHours();
+
+sec.style.transform = `rotate( ${timeDiff( 60, current_sec )}deg )`;
+min.style.transform = `rotate( ${timeDiff( 60, current_min )}deg )`;
+hour.style.transform = `rotate( ${timeDiff( 12, current_hour )}deg )`;
+},1000);
+
+/**
+ * this function for time calculation
+ * @param {*} time 
+ * @param {*} current 
+ * @returns 
+ */
+
+function timeDiff(time, current) {
+  return (360 * current) / time;
+}
+
+
+/**
+ * this function for calculator
+ */
+
+let cal_arr = [];
+
+let getcal = (val)=>{
+  cal_arr.push(val)
+  document.querySelector('.first_display').innerHTML=cal_arr.join('');
+}
+
+let allclear = () => {
+
+  cal_arr = [];
+  document.querySelector('.first_display').innerHTML = 0;
+  document.querySelector('.second_display').innerHTML = 0
+}
+let cancel = () => {
+  cal_arr.pop();
+  document.querySelector('.first_display').innerHTML = cal_arr.join('');
+}
+
+
+
+let finalresult = () => {
+  let call_value = cal_arr.join('');
+  document.querySelector('.second_display').innerHTML = eval(call_value)
+}
